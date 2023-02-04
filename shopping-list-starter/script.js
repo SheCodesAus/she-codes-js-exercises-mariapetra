a7cc380a39208e2a837158248655d6c8d8778836
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-02-05 07:37:59 +0800

    update shopping list to include due date

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,33 +1,47 @@
-let shoppingItems = ["milk", "eggs", "bread"]
+let shoppingItems = [
+    //set array for shopping list and due date
+    //with 2 objects - name and dueDate
+    { name: "milk", dueDate: "01/01/2023" },
+    { name: "eggs", dueDate: "02/01/2023" },
+    { name: "bread", dueDate: "03/01/2023" }
+];
 
 function updateItems() {
-    const listElement = document.getElementById("shopping-list-items")
+    //gets the shopping-list-iyems input from the HTML and sets it to a list
+    const listElement = document.getElementById("shopping-list-items");
+    // sets innerHTML to empty string
     listElement.innerHTML = "";
+    //iterate through the shoppingItems 
     shoppingItems.forEach((item) => {
-        console.log(item)
+        //create a list element (note that a ul has been added in the HTML and it will sit under this)
         let itemElement = document.createElement("li");
-        itemElement.innerText = item;
-        listElement.appendChild(itemElement)
-        
-    })
+        //set the inner text by accessing the shopping list item name and dueDate
+        itemElement.innerText = `${item.name} - Due: ${item.dueDate}`;
+        //append these to the itemElement
+        listElement.appendChild(itemElement);
+    });
 }
-
+//call updateItems function to update your shopping list
 updateItems();
 
-
+//add new items to your shopping list
 function addItem() {
-    //getting value
+    //getting values by accessing the ID in the input in HTML
     const item = document.getElementById("new-item-text").value;
-    shoppingItems.push(item)
-    console.log(shoppingItems)
+    const dueDate = document.getElementById("due-date").value;
+    //push values to the shoppingItems
+    shoppingItems.push({ name: item, dueDate: dueDate });
+    //call function to updateItems
     updateItems();
-    //reset value to empty string
-    item = document.getElementById("new-item-text").value = "";}
-
-function clearItems() {
-    shoppingItems.length = 0;
-    updateItems()
+    //reset values of the item and dueDate input to empty string
+    // note I have included this as one line as JS is read top to bottom
+    // and only one statement would work if they were set seperately
+    document.getElementById("new-item-text").value = document.getElementById("due-date").value = "";
 }
 
-
-
+//clears the shopping list by setting the length of the shoppingItems to 0 
+function clearItems() {
+    shoppingItems.length = 0;
+    //call your updateItems function
+    updateItems();
+}
