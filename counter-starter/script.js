17dfba46ba3915029e5d0f25136ebf103a008799
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-02-05 08:50:15 +0800

    add nultiple and divide function

--- counter-starter/script.js
+++ counter-starter/script.js
@@ -1,15 +1,22 @@
+const numberElement = document.getElementById("number");
+let number = numberElement.innerText;
+
 function add(step) {
-    const numberElement = document.getElementById("number");
-    let number = numberElement.innerText;
     number = parseInt(number) + step;
     numberElement.innerText = number;
-    console.log(number)
 }
 
 function subtract(step) {
-    const numberElement = document.getElementById("number");
-    let number = numberElement.innerText;
     number = parseInt(number) - step;
     numberElement.innerText = number;
-    console.log(number)
+}
+
+function multiply(step) {
+    number = parseInt(number) * step;
+    numberElement.innerText = number;
+}
+
+function divide(step) {
+    number = parseInt(number) / step;
+    numberElement.innerText = number;
 }
